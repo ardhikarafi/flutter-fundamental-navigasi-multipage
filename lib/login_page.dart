0e07ff543c6f109b0c668e1de4bb6912f0d37009
Your Name <you@example.com>
2020-08-07 13:55:18 +0700

    Add Login Page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi_multipage/main_page.dart';
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text("Login"),
+          onPressed: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) {
+              return MainPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
