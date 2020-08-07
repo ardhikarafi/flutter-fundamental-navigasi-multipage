5c770eaf82796cdb01cc802e476e9ebdc544ccff
Your Name <you@example.com>
2020-08-07 13:58:36 +0700

    Add Second Page

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Back Home"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
