d622f98c26144e511fc9ce97876b5753f5a71b1b
Your Name <you@example.com>
2020-08-07 13:53:28 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/navigasi_multipage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigasi_multipage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi_multipage/second_page.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
